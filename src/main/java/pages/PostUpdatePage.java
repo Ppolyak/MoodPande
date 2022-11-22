839bab0999cbb5ece288543703d74119b8ab6189
Ppolyak <pasha_polyakov8@mail.ru>
2022-11-22 18:02:28 +0300

    Changes

---
+++ src/main/java/pages/PostUpdatePage.java
@@ -0,0 +1,37 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class PostUpdatePage {
+
+    /*private static final SelenideElement RATE_XPATH = $(By.xpath("//div[@class='buttons hdas-addons " +
+            "is-hidden-touch']/button[contains(text(),'%s')]"));*/
+
+    /*private static final SelenideElement MOOD_XPATH = $(By.xpath("//div[@class='buttons']" +
+            "/button[contains(text(),'%s')]"));*/
+    private static final SelenideElement MOOD_XPATH = $(By.xpath("//div[@class='buttons']" +
+            "/button[contains(text(),'no')]"));
+    private static final SelenideElement RATE_XPATH = $(By.xpath("//div[@class='buttons hdas-addons " +
+            "is-hidden-touch']/button[contains(text(),'8')]"));
+    private static final SelenideElement UPDATE_BUTTON = $(By.xpath("//a[contains(text(),'Update')]"));
+
+
+    public PostUpdatePage setRate(){
+        RATE_XPATH.click();
+        return this;
+    }
+
+    public PostUpdatePage setMood(){
+        MOOD_XPATH.click();
+        return this;
+    }
+
+    public void clickOnUpdateButton(){
+        UPDATE_BUTTON.click();
+    }
+
+
+}
